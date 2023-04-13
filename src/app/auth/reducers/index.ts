955f4bd16cd4b86040e521667bf69aa960d8e68d
Adolfo da Silveira Poiatti <adolfododo@hotmail.com>
2023-04-12 21:46:48 -0300

    initial commit

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,32 @@
+import {
+  ActionReducerMap, createReducer,
+  MetaReducer, on, State
+} from '@ngrx/store';
+import { environment } from '../../../environments/environment';
+import {User} from '../model/user.model';
+import {AuthActions} from '../action-types';
+
+export const authFeatureKey = 'auth';
+
+export interface AuthState {
+  user: User
+}
+
+export const initialAuthState: AuthState ={
+  user: undefined
+}
+
+
+export const authReducer =  createReducer(
+  initialAuthState,
+  on(AuthActions.login,(state, action) => {
+    return {
+      user: action.user
+    }
+  }),
+  on(AuthActions.logout, (state, action) =>{
+    return {
+      user: undefined
+    }
+  })
+);
